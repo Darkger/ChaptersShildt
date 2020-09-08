0f33d7123def4af42a0ed64637d3bf3766aef1a6
Eugene <borninger@yandex.ru>
2020-09-08 19:22:35 +0300

    +20 and 21 chapters

--- src/main/java/com/eugene/javacore/practic/RegionController.java
+++ src/main/java/com/eugene/javacore/practic/RegionController.java
@@ -7,48 +7,45 @@ import java.util.List;
 public class RegionController {
     public Region save(Region region) throws IOException {
         RegionRepository regionRepository = new RegionRepository();
-        region =regionRepository.save(region);
-        if( region==null)
-            return  null;
+        region = regionRepository.save(region);
+        if (region == null)
+            return null;
         else
-        return  regionRepository.save(region);
+            return regionRepository.save(region);
     }
 
     public void deleteById(String str) throws IOException {
-
         RegionRepository regionRepository = new RegionRepository();
         regionRepository.deleteById(Long.parseLong(str));
     }
 
     public Region getRegionById(String str) throws IOException {
-        RegionView regionView = new RegionView();
         RegionRepository regionRepository = new RegionRepository();
-        Region region =regionRepository.getById(Long.parseLong(str));
-        if(region== null)
+        Region region = regionRepository.getById(Long.parseLong(str));
+        if (region == null)
             return null;
-        else {return region;}
-    }
-
-    public Region  Update(Region region) throws IOException {
-        RegionView regionView = new RegionView();
-        RegionRepository regionRepository = new RegionRepository();
-        region = regionRepository.update(region);
-        if (region!=null) {
+        else {
             return region;
         }
-        else return null;
+    }
+
+    public Region Update(Region region) throws IOException {
+        RegionRepository regionRepository = new RegionRepository();
+        region = regionRepository.update(region);
+        if (region != null) {
+            return region;
+        } else return null;
 
 
     }
 
     public List<Region> getAll() throws IOException {
-        RegionView regionView = new RegionView();
         RegionRepository regionRepository = new RegionRepository();
-        List <Region> regionList =  regionRepository.getAll();
-        if(regionList.isEmpty())
+        List<Region> regionList = regionRepository.getAll();
+        if (regionList.isEmpty())
             return null;
         else
-        return regionList;
+            return regionList;
     }
 
 }
